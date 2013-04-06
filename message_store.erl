e878c86bd4f53d6d145afce134c748b3deb3aa17
Rodrigo Dominguez <rorra@rorra.com.ar>
2013-04-06 04:53:15 -0700

    Storing message for offline users, so they can get it when they login

--- message_store.erl
+++ message_store.erl
@@ -11,14 +11,44 @@
 	 body,
 	 created_on}).
 
+save_message(Addressee, Body) ->
+    global:send(?SERVER, {save_msg, Addressee, Body}).
+
+find_messages(Addressee) ->
+    global:send(?SERVER, {find_msgs, Addressee, self()}),
+    receive
+	{ok, Messages} ->
+	    Messages
+    end.
+
 start() ->
-    server_util:start(?SERVER, {message_store, run, []}).
+    server_util:start(?SERVER, {message_store, run, [true]}).
 
 stop() ->
     server_util:stop(?SERVER).
 
-run() ->
-    ok.
+run(FirstTime) ->
+    if
+	FirstTime == true ->
+	    init_store(),
+	    run(false);
+	true ->
+	    receive
+		{save_msg, Addressee, Body} ->
+		    store_message(Addressee, Body),
+		    run(FirstTime);
+		{find_msgs, Addressee, Pid} ->
+		    Messages = get_messages(Addressee),
+		    Pid ! {ok, Messages},
+		    run(FirstTime);
+		shutdown ->
+		    mnesia:stop(),
+		    io:format("Shutting down...~n");	    
+		Msg ->
+		    io:format("Warning, invalid message received on message_store: ~p~n", [Msg]),
+		    run(FirstTime)
+	    end
+    end.
 
 delete_messages(Messages) ->
     F = fun() ->
@@ -28,12 +58,14 @@ delete_messages(Messages) ->
 
 get_messages(Addressee) ->
     F = fun() ->
-		Query = qlc:q([M#chat_message.body || M <- mnesia:table(chat_message),
-						      M#chat_message.addressee =:= Addressee]),
+		Query = qlc:q([M || M <- mnesia:table(chat_message),
+				    M#chat_message.addressee =:= Addressee]),
 		Results = qlc:e(Query),
 		delete_messages(Results),
-		Results end,
-    mnesia:transaction(F).
+		lists:map(fun(Msg) -> Msg#chat_message.body end, Results) end,
+    {atomic, Messages} = mnesia:transaction(F),
+    Messages.
+    
 
 store_message(Addressee, Body) ->
     F = fun() ->
@@ -45,7 +77,7 @@ init_store() ->
     mnesia:create_schema([node()]),
     mnesia:start(),
     try
-	mnesia:table_info(type, chat_message)
+	mnesia:table_info(chat_message, type)
     catch
 	exit: _ ->
 	    mnesia:create_table(chat_message, [{attributes, record_info(fields, chat_message)},
