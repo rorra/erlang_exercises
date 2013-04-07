929fdf8a4b6aabfc7dd396b9c7cddffa92ee9e2b
Rodrigo Dominguez <rorra@rorra.com.ar>
2013-04-06 18:17:46 -0700

    Some other tests

--- fizzbuzz.erl
+++ fizzbuzz.erl
@@ -2,5 +2,13 @@
 
 -export([analyze/1]).
 
-analyze(1) ->
-    1.
+analyze(N) ->
+    if N rem 15 == 0 ->
+	    fizzbuzz;
+       N rem 3 == 0 ->
+	    fizz;
+       N rem 5 == 0 ->
+	    buzz;
+       true ->
+	    N
+    end.
--- test_fizzbuzz.erl
+++ test_fizzbuzz.erl
@@ -3,4 +3,8 @@
 -include_lib("eunit/include/eunit.hrl").
 
 analyze_test() ->
-    fizzbuzz:analyze(1).
+    ?assertEqual(1, fizzbuzz:analyze(1)),
+    ?assertEqual(fizz, fizzbuzz:analyze(3)),
+    ?assertEqual(buzz, fizzbuzz:analyze(5)),
+    ?assertEqual(fizzbuzz, fizzbuzz:analyze(15)).
+    
